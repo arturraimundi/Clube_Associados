c997b743b4b1e51b370651c8520c666b81f4c3d5
Artur Raimundi <79712639+arturraimundi@users.noreply.github.com>
2024-11-08 17:05:53 -0300

    Add files via upload

---
+++ ClubeAssociados/src/socio.java
@@ -0,0 +1,48 @@
+public class socio {
+    private String nome, sexo;
+    private int qtd_filhos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getQtd_filhos() {
+        return qtd_filhos;
+    }
+
+    public void setQtd_filhos(int qtd_filhos) {
+        this.qtd_filhos = qtd_filhos;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+
+    private float altura, salario;
+
+
+}
